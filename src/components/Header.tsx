86d21d6916c67b8463386c70b264cbf5978bf366
Borys Gajda <51117842+borysgajda@users.noreply.github.com>
2023-01-08 22:30:01 +0100

    Add files via upload

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,35 @@
 import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import myImage from "./calendar.png";
+
+const Nav = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  align-items: center;
+  height: 65px;
+  width: 47.5%;
+  padding: 20px;
+  margin: 20px;
+`;
+
+const Tittle = styled.div`
+  font-size: 1.75rem;
+`;
+const Day = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin: 10px;
+  font-size: 1.25rem;
+`;
+const Project = styled.div`
+  font-size: 0.75rem;
+  font-weight: 500;
+  margin-left: auto;
+`;
+const Image = styled.img`
+  height: 100px;
+`;
 
 export const Header = () => {
   const newDate = useRef(new Date());
@@ -12,26 +43,54 @@ export const Header = () => {
     setCurrentYear(newDate.current.getFullYear());
   }, 1000);
   const dayIndex = new Date().getDay();
-  const getDayName = (dayIndex: number) =>{
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  return days[dayIndex];
-  }
+  const getDayName = (dayIndex: number) => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return days[dayIndex];
+  };
   const monthIndex = new Date().getDay();
   const getMonthName = () => {
     const month = [
-      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",];
-    return month[monthIndex - 4];
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return month[monthIndex];
   };
   const dayName = getDayName(dayIndex);
   const monthName = getMonthName();
-  const day = `${currentDay + 1}`;
+  const day = `${currentDay + 8}`;
   const year = `${currentYear}`;
   return (
-    <header className="header">
-      <h1 className="header--title">{day}</h1>
-      <h5 className="header--dayy"> {dayName} <br></br>{monthName}, {year}</h5>
-      <h4 className="header--project">
-        <img src="calendar.png" className="header--image" alt="Calendar" />
-      </h4>
-    </header>
-  );}
+    <Nav>
+      <Tittle>
+        <h1>{day}</h1>
+      </Tittle>
+      <Day>
+        <h5>
+          {dayName} <br></br>
+          {monthName}, {year}
+        </h5>
+      </Day>
+      <Project>
+        <Image src={myImage}></Image>
+      </Project>
+    </Nav>
+  );
+};
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,26 +1,55 @@
 import React, { useRef } from "react";
-import "./styles.css";
+import Button from "./styled/button";
+import styled from "styled-components";
+import Boxx from "./styled/box";
+
+const Box = styled.input`
+  width: 100%;
+  border-radius: 50px;
+  padding: 20px 30px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-size: 25px;
+  border: none;
+  transition: 0.2s;
+  box-shadow: inset 0 0 5px black;
+
+  &:focus {
+    box-shadow: 0 0 10px 1000px rgba(0, 0, 0, 0.5);
+    outline: none;
+  }
+`;
+const Innput = styled.form`
+  display: flex;
+  width: 47.5%;
+  position: relative;
+  align-items: center;
+`;
+
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
   Add: (e: React.FormEvent) => void;
 }
 const Input = ({ todo, setTodo, Add }: Props) => {
-    const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input" 
-    onSubmit={(e) => {Add(e); inputRef.current?.blur();}}>
-      <input
-        ref ={inputRef}
+    <Innput
+      onSubmit={(e: React.FormEvent<Element>) => {
+        Add(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <Box
+        ref={inputRef}
         placeholder="Wpisz swoje zadanie"
-        className="input--box"
         value={todo}
-        onChange={(e) => setTodo(e.target.value)}
+        onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+          setTodo(e.target.value)
+        }
       />
-      <button className="input--submint" type="submit">
-        +
-      </button>
-    </form>
+      <Button />
+    </Innput>
   );
 };
 export default Input;
