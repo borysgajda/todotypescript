f667ee1771617c3b33c68d1171955ed4a7371eaf
Borys Gajda <51117842+borysgajda@users.noreply.github.com>
2023-01-25 00:25:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,4 @@
 import React, { FC, useState } from "react";
-import "./App.css";
-import { Todo } from "./model";
-import TodoList from "./components/TodoList";
-import AddPage from "./components/AddPage";
-import Input from "./components/Input";
-import { Header } from "./components/Header";
-
-
-const App: FC = () => {
-  const [todo, setTodo] = useState<string>(" ");
-  const [inputs, setInputs] = useState<Todo[]>([]);
-  const Add = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (todo) {
-      setInputs(
-        [
-          ...inputs,
-          {
-          id: Date.now(),
-          todo: todo,
-          isDone: false,
-          },
-        ]
-      );
-      setTodo("");
-    }
-  };
-  return (
-    <div className="App">
-      <Header />
-      <Input todo={todo} setTodo={setTodo} Add={Add} />
-      <TodoList inputs={inputs} setInputs={setInputs} />
-      <AddPage todo={todo} setTodo={setTodo} Add={Add} />
-    </div>
-  );
-};
-export default App;
-=======
-import React, { FC, useState } from "react";
 import { Todo } from "./model";
 import TodoList from "./components/TodoList";
 import Input from "./components/Input";
