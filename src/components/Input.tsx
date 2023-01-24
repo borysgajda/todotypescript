1fa99cb494e39d5c0c8c8e72338a0a1654b1356f
Borys Gajda <51117842+borysgajda@users.noreply.github.com>
2023-01-25 00:26:56 +0100

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,30 +1,4 @@
 import React, { useRef } from "react";
-import "./styles.css";
-interface Props {
-  todo: string;
-  setTodo: React.Dispatch<React.SetStateAction<string>>;
-  Add: (e: React.FormEvent) => void;
-}
-const Input = ({ todo, setTodo, Add }: Props) => {
-    const inputRef = useRef<HTMLInputElement>(null);
-  return (
-    <form className="input" 
-    onSubmit={(e) => {Add(e); inputRef.current?.blur();}}>
-      <input
-        ref ={inputRef}
-        placeholder="Wpisz swoje zadanie"
-        className="input--box"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-      />
-      <button className="input--submint" type="submit">
-        +
-      </button>
-    </form>
-  );
-};
-export default Input;
-import React, { useRef } from "react";
 import * as S from "../styled";
 
 interface Props {
@@ -43,7 +17,7 @@ const Input = ({ todo, setTodo, Add }: Props) => {
     >
       <S.Box
         ref={inputRef}
-        placeholder="Wpisz swoje zadanie"
+        placeholder="Enter your task"
         value={todo}
         onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
           setTodo(e.target.value)
